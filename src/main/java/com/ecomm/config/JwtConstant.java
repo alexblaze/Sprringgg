6aea86dd524d674126466ad8dd872b590e424111
sanjay.thakur <sanjay.thakur@dghub.io>
2023-09-20 20:50:42 +0545

    jwt

---
+++ src/main/java/com/ecomm/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.ecomm.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="testksdjfhkjkdfjsdfsdfsdfcvdsfg";
+    public static final String JWT_HEADER="Authorization";
+}
